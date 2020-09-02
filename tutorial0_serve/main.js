e86d33406c2163574042b468b46f533829d86aef
yasth <yasth@yasth.org>
2020-09-01 21:21:30 -0400

    Update main.js

---
+++ tutorial0_serve/main.js
@@ -0,0 +1,3 @@
+console.log("Cuny Dava Vis 2020 Fall run at", new Date().toLocaleString())
+console.log("Running at ",  window.screen.width, " x ", window.screen.height)
+console.log(document.body.innerText)
